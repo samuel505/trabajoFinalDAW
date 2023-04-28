4394ec85f4c2bea072011819d124d8f45e34a57c
samuel505 <yosamuelbarros@gmail.com>
2023-04-29 01:14:17 +0200

    fixed bug visual favoritos and nuevo database

--- proxecto.sql
+++ proxecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 28-04-2023 a las 01:26:45
+-- Tiempo de generación: 29-04-2023 a las 01:13:22
 -- Versión del servidor: 10.6.7-MariaDB-2ubuntu1.1
 -- Versión de PHP: 8.1.9
 
@@ -46,12 +46,7 @@ CREATE TABLE `archivos` (
 --
 
 INSERT INTO `archivos` (`id_archivo`, `filecode`, `id_usuario`, `ruta_local`, `nombre_archivo`, `fecha_subida`, `fecha_borrado`, `type`, `size`, `borrado`, `favorito`) VALUES
-(324, '6449c1cf1894f', 182, 'uploads/6449c1cf1894f', 'logo (1) (1)', '2023-04-27', NULL, '', 217295, 0, 0),
-(326, '644ae766de567', 0, 'uploads/644ae766de567.jpeg', 'image (6) (1).jpeg', '2023-04-27', NULL, 'jpeg', 48705, 0, 0),
-(327, '644af1306d10c', 0, 'uploads/644af1306d10c.jpeg', 'image (6) (1).jpeg', '2023-04-27', NULL, 'jpeg', 48705, 0, 0),
-(328, '644af17a6575d', 0, 'uploads/644af17a6575d.png', 'Valores-calidad (1).png', '2023-04-27', NULL, 'png', 82568, 0, 0),
-(329, '644af2eb6167c', 0, 'uploads/644af2eb6167c.png', 'Valores-calidad (1).png', '2023-04-27', NULL, 'png', 82568, 0, 0),
-(330, '644af42340c36', 0, 'uploads/644af42340c36', 'logo (1) (1) (5)', '2023-04-27', NULL, '', 217295, 0, 0);
+(324, '6449c1cf1894f', 182, 'uploads/6449c1cf1894f', 'logo (1) (1)', '2023-04-27', NULL, '', 217295, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -100,7 +95,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `email`, `nombre`, `apellidos`, `password`, `genero`, `pais`, `fecha_nacimiento`, `image`, `id_plan`) VALUES
-(0, 'test@test.com', 'test', 'test', '$2y$10$EyyT5.TW7YtszI7/2pdfsOZ7.n27RqmHEyIzegDmVFVQ27EP3AJV6', 'hombre', 'ES', '2024-01-26', 'profiles/644addc38f543.jpg', 0),
+(0, 'test@test.com', 'test', 'test', '$2y$10$zqE/n5dVhLGD0Cdx47oRPuSdeZlK/SWajlEtT3c9d//7bmCcJaoT6', 'hombre', 'ES', '2024-01-26', 'profiles/644addc38f543.jpg', 0),
 (1, 'test2@test.com', 'test2', 'test2', '$2y$10$A/zMuHx63ZHk6cre6p7zXerh0J7idJ8mFlqocQsfxvc2IhBlkVZu.', 'NULL', 'PT', '0000-00-00', '', 0),
 (182, 'test3@test.com', 'test3', 'test3', '$2y$10$T7P9Z/D6BFj/NJxEk4UQM.nHmZTC9ZS.nu74.M0nwRDR4QJVgy9aq', 'hombre', NULL, NULL, 'profiles/6449b25945969.png', 0);
 
@@ -136,7 +131,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `archivos`
 --
 ALTER TABLE `archivos`
-  MODIFY `id_archivo` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=331;
+  MODIFY `id_archivo` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=334;
 
 --
 -- AUTO_INCREMENT de la tabla `planes`
