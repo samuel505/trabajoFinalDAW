718bf5371b0068f13c675115937796d49f5c4064
samuel505 <yosamuelbarros@gmail.com>
2023-05-22 22:09:22 +0200

    añadido confirmaciones de borrado

--- app/Views/trash_view.php
+++ app/Views/trash_view.php
@@ -23,12 +23,12 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                    <div class="alert text-white bg-danger" role="alert" style="display:none;" id="error">
-                                <div class="text-center text-uppercase iq-alert-text"></div>
-                            </div>
-                            <div class="alert text-white bg-success" role="alert" style="display:none;" id="success">
-                                <div class="text-center text-uppercase iq-alert-text"></div>
-                            </div>
+                        <div class="alert text-white bg-danger" role="alert" style="display:none;" id="error">
+                            <div class="text-center text-uppercase iq-alert-text"></div>
+                        </div>
+                        <div class="alert text-white bg-success" role="alert" style="display:none;" id="success">
+                            <div class="text-center text-uppercase iq-alert-text"></div>
+                        </div>
                         <div class="card card-block card-stretch  card-transparent">
                             <div class="card-header d-flex justify-content-between pb-0">
                                 <div class="header-title">
@@ -36,7 +36,7 @@
                                 </div>
                                 <div class="d-flex align-items-center">
                                     <div class="mr-4">
-                                        <button class="btn btn-primary " onclick="permanentDeleteAll()">Vaciar papelera</button>
+                                        <button class="btn btn-primary " onclick="mostrarModalConfirmacion2()">Vaciar papelera</button>
                                     </div>
                                 </div>
                             </div>
@@ -77,7 +77,7 @@
                                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton003" style>
                                                             <span hidden class="deleted"><?= $archivo['filecode'] ?></span>
                                                             <button class="dropdown-item" onclick="recoverFile(this)"><i class="ri-restart-line mr-2"></i>Restaurar</button>
-                                                            <button class="dropdown-item" onclick="permanentDelete(this)"><i class="ri-delete-bin-6-fill mr-2"></i>Borrado permanente</button>
+                                                            <button class="dropdown-item" onclick="mostrarModalConfirmacion()" id="borrarFile"><i class="ri-delete-bin-6-fill mr-2"></i>Borrado permanente</button>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -92,7 +92,54 @@
             </div>
         </div>
     </div>
+    <!-- Modal de confirmación -->
+    <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmModalLabel">¿Seguro de querer borrar permanentemente este archivo?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Una vez borrado no se podrá recuperar.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" onclick="permanentDelete()">Confirmar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="confirmModal2" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel2" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmModalLabel2">¿Estas seguro de querer vaciar la papelera?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Una vez vaciada la papelera, no se podran recuperar los archivos.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary" onclick="permanentDeleteAll()">Confirmar</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script>
+        function mostrarModalConfirmacion() {
+            $('#confirmModal').modal('show');
+        }
+
+        function mostrarModalConfirmacion2() {
+            $('#confirmModal2').modal('show');
+        }
+
         function recoverFile(boton) {
 
             let filecode = boton.previousElementSibling.innerHTML;
@@ -133,7 +180,7 @@
                                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton003" style>
                                                         <span hidden class="deleted">${archivo['filecode']}</span>
                                                         <button class="dropdown-item" onclick="recoverFile(this)"><i class="ri-restart-line mr-2"></i>Restaurar</button>
-                                                        <button class="dropdown-item" onclick="permanentDelete(this)"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
+                                                        <button class="dropdown-item" onclick="mostrarModalConfirmacion()" id="borrar"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -163,32 +210,31 @@
         }
 
 
-        function permanentDelete(boton) {
+        function permanentDelete() {
+            let boton = document.getElementById("borrar");
+            $('#confirmModal').modal('hide');
+            let filecode = boton.previousElementSibling.previousElementSibling.innerHTML;
 
+            $.ajax({
+                type: "POST",
+                url: '/permanentDelete',
+                dataType: 'json',
+                data: {
+                    "filecode": filecode
+                },
+                success: function(array) {
 
-            if (confirmDelete()) {
-                let filecode = boton.previousElementSibling.previousElementSibling.innerHTML;
+                    array1 = array['archivos'];
+                    let URLactual = window.location.origin;
+                    let modalError = document.getElementById("error");
+                    let success = document.getElementById("success");
+                    let string = "";
+                    if (typeof(array1) !== 'undefined') {
 
-                $.ajax({
-                    type: "POST",
-                    url: '/permanentDelete',
-                    dataType: 'json',
-                    data: {
-                        "filecode": filecode
-                    },
-                    success: function(array) {
+                        for (let i = 0; i < array1.length; i++) {
+                            let archivo = array1[i];
 
-                        array1 = array['archivos'];
-                        let URLactual = window.location.origin;
-                        let modalError = document.getElementById("error");
-                        let success = document.getElementById("success");
-                        let string = "";
-                        if (typeof(array1) !== 'undefined') {
-
-                            for (let i = 0; i < array1.length; i++) {
-                                let archivo = array1[i];
-
-                                string += `<div class="col-lg-3 col-md-6 col-sm-6">
+                            string += `<div class="col-lg-3 col-md-6 col-sm-6">
                                 <div class="card card-block card-stretch card-height">
                                     <div class="card-body image-thumb">
                                         <div class="mb-4 text-center p-3 rounded iq-thumb">
@@ -205,7 +251,7 @@
                                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton003" style>
                                                         <span hidden class="deleted">${archivo['filecode']}</span>
                                                         <button class="dropdown-item" onclick="recoverFile(this)"><i class="ri-restart-line mr-2"></i>Restaurar</button>
-                                                        <button class="dropdown-item" onclick="permanentDelete(this)"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
+                                                        <button class="dropdown-item" onclick="mostrarModalConfirmacion()" id="borrar"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -213,81 +259,72 @@
                                     </div>
                                 </div>
                             </div>`
-
-                            }
                         }
-
-                        let n1 = (100 - ((array['TotalSize'] - array['OccupiedSize']) / array['TotalSize']) * 100).toFixed(3);
-
-                        let n2 = (array['TotalSize'] - array['OccupiedSize']).toFixed(3);
-                        lMenu = '<h4 class="mb-3"><i class="las la-cloud mr-2"></i>Almacenamiento</h4> <p>' + array['OccupiedSize'].toFixed(3) + '/' + array['TotalSize'] + 'GB Usado</p> <div class="iq-progress-bar mb-3"> <span class="bg-primary iq-progress progress-1" style="width: ' + n1 + '%"> </span> </div> <p> ' + n1 + '% Full - ' + n2 + ' GB Free</p> <a href="#" class="btn btn-outline-primary view-more mt-4">Ampliar espacio</a> </div> <div class="p-3"></div> </div> </div>';
-                        leftMenu = document.getElementById('leftMenu');
-                        archivos = document.getElementById('archivos');
-                        archivos.innerHTML = string;
-
-                        leftMenu.innerHTML = lMenu;
                     }
-                }).fail(function(array) {
-                    let response = JSON.parse(array.responseText);
-                    let modalError = document.getElementById("error");
-                    let modalsuccess = document.getElementById("success");
-                    modalsuccess.style.display = "none";
-                    modalError.style.display = "block";
-                    modalError.firstChild.nodeValue = response;
-                    setTimeout(() => {
-                        modalError.style.display = "none"
-                    }, 3000);
-                });
 
-            }
+                    let n1 = (100 - ((array['TotalSize'] - array['OccupiedSize']) / array['TotalSize']) * 100).toFixed(3);
+
+                    let n2 = (array['TotalSize'] - array['OccupiedSize']).toFixed(3);
+                    lMenu = '<h4 class="mb-3"><i class="las la-cloud mr-2"></i>Almacenamiento</h4> <p>' + array['OccupiedSize'].toFixed(3) + '/' + array['TotalSize'] + 'GB Usado</p> <div class="iq-progress-bar mb-3"> <span class="bg-primary iq-progress progress-1" style="width: ' + n1 + '%"> </span> </div> <p> ' + n1 + '% Full - ' + n2 + ' GB Free</p> <a href="#" class="btn btn-outline-primary view-more mt-4">Ampliar espacio</a> </div> <div class="p-3"></div> </div> </div>';
+                    leftMenu = document.getElementById('leftMenu');
+                    archivos = document.getElementById('archivos');
+                    archivos.innerHTML = string;
+
+                    leftMenu.innerHTML = lMenu;
+                }
+            }).fail(function(array) {
+                let response = JSON.parse(array.responseText);
+                let modalError = document.getElementById("error");
+                let modalsuccess = document.getElementById("success");
+                modalsuccess.style.display = "none";
+                modalError.style.display = "block";
+                modalError.firstChild.nodeValue = response;
+                setTimeout(() => {
+                    modalError.style.display = "none"
+                }, 3000);
+            });
+
+
         }
 
 
         function permanentDeleteAll() {
             let modalError = document.getElementById("error");
             let success = document.getElementById("success");
+            $('#confirmModal2').modal('hide');
 
-            if (confirmDelete()) {
+            $.ajax({
+                type: "POST",
+                url: '/permanentDeleteAll',
+                dataType: 'json',
 
+                success: function(array) {
 
-                $.ajax({
-                    type: "POST",
-                    url: '/permanentDeleteAll',
-                    dataType: 'json',
+                    array1 = array['archivos'];
+                    let n1 = (100 - ((array['TotalSize'] - array['OccupiedSize']) / array['TotalSize']) * 100).toFixed(3);
+                    let n2 = (array['TotalSize'] - array['OccupiedSize']).toFixed(3);
 
-                    success: function(array) {
+                    lMenu = '<h4 class="mb-3"><i class="las la-cloud mr-2"></i>Almacenamiento</h4> <p>' + array['OccupiedSize'].toFixed(3) + '/' + array['TotalSize'] + 'GB Usado</p> <div class="iq-progress-bar mb-3"> <span class="bg-primary iq-progress progress-1" style="width: ' + n1 + '%"> </span> </div> <p> ' + n1 + '% Full - ' + n2 + ' GB Free</p> <a href="#" class="btn btn-outline-primary view-more mt-4">Ampliar espacio</a> </div> <div class="p-3"></div> </div> </div>';
+                    leftMenu = document.getElementById('leftMenu');
+                    archivos = document.getElementById('archivos');
 
-                        array1 = array['archivos'];
-                        let n1 = (100 - ((array['TotalSize'] - array['OccupiedSize']) / array['TotalSize']) * 100).toFixed(3);
-                        let n2 = (array['TotalSize'] - array['OccupiedSize']).toFixed(3);
+                    archivos.innerHTML = "";
+                    leftMenu.innerHTML = lMenu;
 
-                        lMenu = '<h4 class="mb-3"><i class="las la-cloud mr-2"></i>Almacenamiento</h4> <p>' + array['OccupiedSize'].toFixed(3) + '/' + array['TotalSize'] + 'GB Usado</p> <div class="iq-progress-bar mb-3"> <span class="bg-primary iq-progress progress-1" style="width: ' + n1 + '%"> </span> </div> <p> ' + n1 + '% Full - ' + n2 + ' GB Free</p> <a href="#" class="btn btn-outline-primary view-more mt-4">Ampliar espacio</a> </div> <div class="p-3"></div> </div> </div>';
-                        leftMenu = document.getElementById('leftMenu');
-                        archivos = document.getElementById('archivos');
-
-                        archivos.innerHTML = "";
-                        leftMenu.innerHTML = lMenu;
-
-                    }
-                }).fail(function(array) {
-                    let response = JSON.parse(array.responseText);
-                    let modalError = document.getElementById("error");
-                    let modalsuccess = document.getElementById("success");
-                    successModal.style.display = "none";
-                    modalError.style.display = "block";
-                    modalError.firstChild.nodeValue = response;
-                    setTimeout(() => {
-                        modalError.style.display = "none"
-                    }, 3000);
-                });
-
-            }
-        }
+                }
+            }).fail(function(array) {
+                let response = JSON.parse(array.responseText);
+                let modalError = document.getElementById("error");
+                let modalsuccess = document.getElementById("success");
+                successModal.style.display = "none";
+                modalError.style.display = "block";
+                modalError.firstChild.nodeValue = response;
+                setTimeout(() => {
+                    modalError.style.display = "none"
+                }, 3000);
+            });
 
 
-        function confirmDelete() {
-
-            return true;
         }
     </script>
     <script>
@@ -326,7 +363,7 @@
                                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton003" style>
                                                         <span hidden class="deleted">${archivo['filecode']}</span>
                                                         <button class="dropdown-item" onclick="recoverFile(this)"><i class="ri-restart-line mr-2"></i>Restaurar</button>
-                                                        <button class="dropdown-item" onclick="permanentDelete(this)"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
+                                                        <button class="dropdown-item" onclick="mostrarModalConfirmacion()" id="borrar"><i class="ri-delete-bin-6-fill mr-2" ></i>Borrado permanente</button>
                                                     </div>
                                                 </div>
                                             </div>
